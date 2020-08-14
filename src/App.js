72c1f4e852db971e1fce964b1712dcb6c1c0c391
a970747 <Matt.J.Jackson@gmail.com>
2020-08-14 04:57:11 -0400

    Complete major features and functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 import dictionary from './dictionary';
-// import DisplayDefs from './DisplayDefs'
+import DisplayDefs from './DisplayDefs'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      dictionaryLength: '',
       definitionList: [],
       correctWords: [],
       missedWords: [],
@@ -18,31 +17,20 @@ class App extends Component {
     }
   }
 
-  dictionaryLength = () => {
-    this.setState({
-      dictionaryLength: dictionary.length,
-  }, () => {console.log(this.state.dictionaryLength)})
-  }
 
   setQuery = () => {
+    let number = Math.floor(Math.random() * dictionary.length)
     this.setState({
-        queryWord: dictionary[Math.floor(Math.random() * dictionary.length)]
-    }, () => {console.log(this.state.queryWord)})
-}
-
-componentDidMount() {
-    this.setQuery();
-    this.dictionaryLength();
-}
-
-  nextWord = () => {
-    this.setState({
-      userInput: '',
-      rightWrong: true,
-    }, () => {console.log(this.state.rightWrong)})
-    this.setQuery();
+        queryWord: dictionary[number]
+    })
   }
 
+  componentDidMount() {
+      this.setQuery();
+  }
+
+  //handles the user input for state as it's input.
+  //includes an if/else statement to deal with people attempting to put in spaces.
   handleChange = (event) => {
     if (event.currentTarget.value.includes(' ')) {
       event.currentTarget.value = event.currentTarget.value.replace(/\s/g, '');
@@ -52,6 +40,23 @@ componentDidMount() {
       });
   }
 
+  //used in the handleSubmit function below.
+  //push the correctly guessed word into a "correct" array for display further down the page.
+  //reset the user input, reset the right/wrong array and get anew word.
+  nextWord = () => {
+    let correctWords = this.state.correctWords;
+    correctWords.push(this.state.queryWord);
+    this.setState({
+      correctWords: correctWords,
+      userInput: '',
+      rightWrong: true,
+    })
+    this.setQuery();
+  }
+
+  //handles the check answer button
+  // if the answer matches the word we are guessing, awesome! Run the nextWord function to get a new word and push the currect word into a correctly guessed array for display.
+  // else, reset the user input and let them know the guess was wrong.
   handleSubmit = (event) => {
     event.preventDefault();
     this.state.userInput === this.state.queryWord
@@ -61,31 +66,76 @@ componentDidMount() {
     this.setState({
       userInput: '',
       rightWrong: false,
-    }, () => {console.log(this.state.rightWrong)})
+    })
   }
 
+  //Handles the submit button for "I give up". It takes the word, pushes it into a "missed" array for display on the page,
+  //and gets you a new word to guess. Resets the right/wrong array so correct/incorrect doesn't stay on page.
   handleQuit = (event) => {
     event.preventDefault();
+    let missedWords = this.state.missedWords;
+    missedWords.push(this.state.queryWord);
+    this.setState({
+      rightWrong: null,
+      missedWords: missedWords,
+    })
+    this.setQuery();
+  }
 
+  //occasionally the definitions are such that you need to take the base, or root form of the word as the supplied word is a plural, or doesn't have a useful definition. 
+  //In these cases we need to change the word we test against to the base. This takes the child prop and sets it in the apps state.
+  rebaseWord = (updatedWord) => {
+    this.setState({
+      queryWord: updatedWord,
+    })
   }
 
   render() {
     return (
-      <div className="App">
-        {/* <DisplayDefs /> */}
+      <div className="App wrapper">
+        <h1>Guess the word!</h1>
+        <h2>Use the form below to guess the word based on the following definitions:</h2>
+        <div className="displayDefBorder">
+          <DisplayDefs queryProp={this.state.queryWord} rebaseWord={this.rebaseWord} catchFunction={this.setQuery}/>
+        </div>
         <form className='flexForm' action="submit">
-          <label htmlFor="answer">Guess the word!</label>
-          <input onChange={this.handleChange} value={this.state.userInput} type="text" id="answer" placeholder={`${this.state.queryWord} - word is ${this.state.queryWord.length} letters long`}/>
+          <label htmlFor="answer" className="sr-only">Guessing form</label>
+          <input onChange={this.handleChange} value={this.state.userInput} type="text" id="answer" placeholder={`First letter '${this.state.queryWord[0]}' - ${this.state.queryWord.length} letters`}/>
           <button onClick={this.handleSubmit} name="check-answer" value="check-answer" > Check Answer</button>
           <button onClick={this.handleQuit} name="give-up" value="give-up"> Give up</button>
         </form>
         {
-          this.state.rightWrong === true ? <h3>correct!</h3> : <h3>incorrect!</h3>
+          this.state.rightWrong === null ? null : this.state.rightWrong === true ? <h3>Correct!</h3> : <h3>Incorrect, Try again!</h3>
         }
+        <div className='listDiv'>
+        {
+          <div>
+            <h4>Correct Answers</h4>
+            <ul className='correctList'>
+            {this.state.correctWords.map((each)=>{
+              return(
+                <li><p>{each}</p></li>
+              )
+            })}
+            </ul>
+          </div>
+        }
+        {
+          <div>
+            <h4>Missed Words</h4>
+            <ul className='missedList'>
+            {this.state.missedWords.map((each)=>{
+              return(
+                <li><p>{each}</p></li>
+              )
+            })}
+            </ul>
+          </div>
+        }
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
--- src/DisplayDefs.jsx
+++ src/DisplayDefs.jsx
@@ -1,5 +1,5 @@
+/* eslint-disable array-callback-return */
 import React, { Component } from 'react';
-import dictionary from './dictionary';
 import axios from 'axios';
 
 class DisplayDefs extends Component {
@@ -10,59 +10,47 @@ class DisplayDefs extends Component {
             definitionArray: [],
         }
     }
-
-    setQuery = () => {
-        this.setState({
-            queryWord: dictionary[Math.floor(Math.random() * dictionary.length)]
-        }, () => {
+    
+    componentDidUpdate(prevProps) {
+        if (prevProps.queryProp !== this.props.queryProp) {
+            this.setState({
+                queryWord: this.props.queryProp,
+            })
             this.setDefinitions();
-        })
+        }
     }
 
-    componentDidMount() {
-        this.setQuery();
-    }
-    // ${this.state.queryWord}
     setDefinitions = () => {
         axios({
-            url: `https://www.dictionaryapi.com/api/v3/references/sd3/json/${this.state.queryWord}`,
+            url: `https://www.dictionaryapi.com/api/v3/references/sd3/json/${this.props.queryProp}`,
             method: 'GET',
             dataType: 'json',
             params: {
                 key: '2d1e7e13-d6a9-4925-80d7-eb9a0228d59f',
             }
         }).then( (response) => {
-            // console.log(this.state.queryWord)
-            // console.log(response.data)
             let filteredArray = response.data.filter((each) => {
-                // console.log(each.hwi.hw.replace(/\*/g,''), this.state.queryWord)
                 if(each.hwi.hw.replace(/\*/g,'') === this.state.queryWord) {
-                    console.log('true')
                     return each
                 }
             })
             this.setState({
                 definitionArray: filteredArray
-            }, () => {
-                // console.log(this.state.definitionArray)
             })
             
             if(filteredArray.length === 0) {
                 filteredArray = response.data.filter((each) => {
                     if (each.meta.stems.includes(this.state.queryWord) === true && each.hwi.hw.replace(/\*/g,'') !== this.state.queryWord) {
-                        console.log('false');
                         return each
                     }
                 })
+                this.props.rebaseWord(filteredArray[0].hwi.hw.replace(/\*/g,''));
                 this.setState({                    
                     queryWord: filteredArray[0].hwi.hw.replace(/\*/g,''),
                     definitionArray: filteredArray
-                }, () => {
-                    console.log(this.state.queryWord)
                 })
             }
-            console.log(this.state.queryWord, this.state.definitionArray)
-        }).catch(() => this.setQuery)
+        }).catch(() => this.props.catchFunction);
     }
 
     render() {
@@ -72,10 +60,12 @@ class DisplayDefs extends Component {
                     this.state.definitionArray.map((each,index) => {
                         return (
                         <>
-                        <ul key={index}>{each.fl}</ul>
-                        {each.shortdef.map((s)=>{
-                            return <li>{s}</li>
+                        <h3>{each.fl}</h3>
+                        <ul>
+                        {each.shortdef.map((s,index)=>{
+                            return <li className="listStyle"><p>{s}</p></li>
                         })}
+                        </ul>
                         </>
                         )                       
                     })
@@ -84,11 +74,5 @@ class DisplayDefs extends Component {
         )
     }
 }
-//! goes undernear the UL before </>
-// {each.shortdef.map((s)=>{
-//     return  (
-//         <li>{s}</li>
-//         )})}
-//! ends here
 
 export default DisplayDefs;
