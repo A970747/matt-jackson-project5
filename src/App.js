23ef41fac2081b5bc5ccdeaef3d80f5cbcf0c616
a970747 <Matt.J.Jackson@gmail.com>
2020-08-13 23:20:06 -0400

    Add initial page setup

--- src/App.js
+++ src/App.js
@@ -1,55 +1,91 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-// import dictionary from './dictionary';
-// import Form from './Form'
-import DisplayDefs from './DisplayDefs'
+import dictionary from './dictionary';
+// import DisplayDefs from './DisplayDefs'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      word: '',
+      dictionaryLength: '',
       definitionList: [],
-      testingWordList: [],
-      testingDefinitionList: [],
+      correctWords: [],
+      missedWords: [],
+      rightWrong: null,
       queryWord: '',
+      userInput: '',
     }
   }
 
+  dictionaryLength = () => {
+    this.setState({
+      dictionaryLength: dictionary.length,
+  }, () => {console.log(this.state.dictionaryLength)})
+  }
 
-  // handleChange = (event) => {
-  //   this.setState({
-  //     userInput: event.target.value
-  //     }, () => {
-  //       always one input behind.
-  //       console.log(this.state.userInput);
-  //       })
-  // }
+  setQuery = () => {
+    this.setState({
+        queryWord: dictionary[Math.floor(Math.random() * dictionary.length)]
+    }, () => {console.log(this.state.queryWord)})
+}
 
-  // handleClick = (event) => {
-  //   event.preventDefault();
-  //   const dbRef = firebase.database().ref();
-  //   dbRef.push(this.state.userInput);
-  //   this.setState({
-  //     userInput: ''
-  //   })
-  // }
+componentDidMount() {
+    this.setQuery();
+    this.dictionaryLength();
+}
+
+  nextWord = () => {
+    this.setState({
+      userInput: '',
+      rightWrong: true,
+    }, () => {console.log(this.state.rightWrong)})
+    this.setQuery();
+  }
+
+  handleChange = (event) => {
+    if (event.currentTarget.value.includes(' ')) {
+      event.currentTarget.value = event.currentTarget.value.replace(/\s/g, '');
+    }
+    this.setState({
+      userInput: event.target.value.toLowerCase(),
+      });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.state.userInput === this.state.queryWord
+    ? 
+      this.nextWord()
+    : 
+    this.setState({
+      userInput: '',
+      rightWrong: false,
+    }, () => {console.log(this.state.rightWrong)})
+  }
+
+  handleQuit = (event) => {
+    event.preventDefault();
+
+  }
 
   render() {
     return (
       <div className="App">
-        <DisplayDefs />
-        <form action="submit">
-          <label htmlFor="answer">Add a book to your bookshelf</label>
-          <input onChange={this.handleChange} value={this.state.userInput} type="text" id="answer"/>
-          {/* <button onClick={this.handleClick} > Give up</button> */}
+        {/* <DisplayDefs /> */}
+        <form className='flexForm' action="submit">
+          <label htmlFor="answer">Guess the word!</label>
+          <input onChange={this.handleChange} value={this.state.userInput} type="text" id="answer" placeholder={`${this.state.queryWord} - word is ${this.state.queryWord.length} letters long`}/>
+          <button onClick={this.handleSubmit} name="check-answer" value="check-answer" > Check Answer</button>
+          <button onClick={this.handleQuit} name="give-up" value="give-up"> Give up</button>
         </form>
+        {
+          this.state.rightWrong === true ? <h3>correct!</h3> : <h3>incorrect!</h3>
+        }
       </div>
     );
   }
 }
-
 
 
 export default App;
--- src/DisplayDefs.jsx
+++ src/DisplayDefs.jsx
@@ -15,15 +15,15 @@ class DisplayDefs extends Component {
         this.setState({
             queryWord: dictionary[Math.floor(Math.random() * dictionary.length)]
         }, () => {
-            this.getDefinitions();
+            this.setDefinitions();
         })
     }
 
     componentDidMount() {
         this.setQuery();
     }
-
-    getDefinitions = () => {
+    // ${this.state.queryWord}
+    setDefinitions = () => {
         axios({
             url: `https://www.dictionaryapi.com/api/v3/references/sd3/json/${this.state.queryWord}`,
             method: 'GET',
@@ -32,49 +32,63 @@ class DisplayDefs extends Component {
                 key: '2d1e7e13-d6a9-4925-80d7-eb9a0228d59f',
             }
         }).then( (response) => {
-            console.log(this.state.queryWord)
-            console.log(response.data)
-            
+            // console.log(this.state.queryWord)
+            // console.log(response.data)
             let filteredArray = response.data.filter((each) => {
-                console.log(each.hwi.hw.replace('*',''), this.state.queryWord)
-                if(each.hwi.hw.replace('*','') === this.state.queryWord) {
+                // console.log(each.hwi.hw.replace(/\*/g,''), this.state.queryWord)
+                if(each.hwi.hw.replace(/\*/g,'') === this.state.queryWord) {
                     console.log('true')
                     return each
                 }
             })
-            console.log(filteredArray);
             this.setState({
                 definitionArray: filteredArray
             }, () => {
-                console.log(this.definitionArray, this.state.queryWord)
+                // console.log(this.state.definitionArray)
             })
-
             
             if(filteredArray.length === 0) {
                 filteredArray = response.data.filter((each) => {
-                    if (each.meta.stems.includes(this.state.queryWord) === true && each.hwi.hw.replace('*','') !== this.state.queryWord) {
+                    if (each.meta.stems.includes(this.state.queryWord) === true && each.hwi.hw.replace(/\*/g,'') !== this.state.queryWord) {
+                        console.log('false');
                         return each
                     }
                 })
-                console.log(filteredArray);
                 this.setState({                    
-                    // queryWord: each.hwi.hw.replace('*',''),
+                    queryWord: filteredArray[0].hwi.hw.replace(/\*/g,''),
                     definitionArray: filteredArray
                 }, () => {
                     console.log(this.state.queryWord)
                 })
-                // this.state.queryWord = each.hwi.hw.replace('*','');
             }
-        })
+            console.log(this.state.queryWord, this.state.definitionArray)
+        }).catch(() => this.setQuery)
     }
 
     render() {
         return(
             <div>
-
+                {
+                    this.state.definitionArray.map((each,index) => {
+                        return (
+                        <>
+                        <ul key={index}>{each.fl}</ul>
+                        {each.shortdef.map((s)=>{
+                            return <li>{s}</li>
+                        })}
+                        </>
+                        )                       
+                    })
+                }
             </div>
         )
     }
 }
+//! goes undernear the UL before </>
+// {each.shortdef.map((s)=>{
+//     return  (
+//         <li>{s}</li>
+//         )})}
+//! ends here
 
 export default DisplayDefs;
