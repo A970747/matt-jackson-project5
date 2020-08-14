884e6e427ea4eb896a06d221dedf1c570cd9437e
a970747 <Matt.J.Jackson@gmail.com>
2020-08-14 05:27:42 -0400

    Updates

---
+++ precache-manifest.0f5f15fc8d048ba84e19bf4f9a91c5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ce1fcc6d3c5300c182de8e895c6254",
+    "url": "/matt-jackson-project5/index.html"
+  },
+  {
+    "revision": "057a54725fe1d5228435",
+    "url": "/matt-jackson-project5/static/css/main.6825cbe2.chunk.css"
+  },
+  {
+    "revision": "971166c11f34a0c1fe6c",
+    "url": "/matt-jackson-project5/static/js/2.2300e6f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/matt-jackson-project5/static/js/2.2300e6f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057a54725fe1d5228435",
+    "url": "/matt-jackson-project5/static/js/main.1cd81684.chunk.js"
+  },
+  {
+    "revision": "a452cae46277531ca332",
+    "url": "/matt-jackson-project5/static/js/runtime-main.05e6c08b.js"
+  }
+]);
